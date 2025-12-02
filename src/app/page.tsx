53ac911ec6106913fc74c7d7d50ca227634acab4
hoangha297 <ha.dh290702@gmail.com>
2025-12-02 14:49:17 +0200

    Setup Inngest to handle background job - add mprocs to run multiple command

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,32 @@
+'use client';
 import { Button } from '@/components/ui/button';
 import { authClient } from '@/lib/auth/auth-client';
 import { requireAuth } from '@/lib/auth/auth-utils';
+import { useTRPC } from '@/trpc/client';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { toast } from 'sonner';
 
-const Home = async () => {
-        await requireAuth();
+const Home = () => {
+        const trpc = useTRPC();
+        const queryClient = useQueryClient();
+        const { data } = useQuery(trpc.getWorkflows.queryOptions());
+        const createWorkflow = useMutation(
+                trpc.createWorkflow.mutationOptions({
+                        onSuccess: () => {
+                                queryClient.invalidateQueries(trpc.getWorkflows.queryOptions());
+                                toast.success('Workflow created successfully');
+                        },
+                        onError: (error) => {
+                                toast.error(error.message);
+                        },
+                }),
+        );
         return (
-                <div
-                        onClick={() => {
-                                authClient.signOut();
-                        }}
-                >
-                        Protected Page
+                <div className="min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6">
+                        {JSON.stringify(data)}
+                        <Button disabled={createWorkflow.isPending} onClick={() => createWorkflow.mutate()}>
+                                Create Workflow
+                        </Button>
                 </div>
         );
 };
